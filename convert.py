d42619dbbec3e61c49a7c15d4f005d8206bbceb6
Felipe Castro <felipecpc@gmail.com>
2019-02-16 12:01:13 +0100

    fix: Support to python 3

--- convert.py
+++ convert.py
@@ -23,10 +23,10 @@ def convert(size, box):
 """-------------------------------------------------------------------""" 
 
 """ Configure Paths"""   
-mypath = "./Labels/006/"
+mypath = "./Labels/001/"
 outpath = "./Labels/output/"
 
-cls = "006"
+cls = "001"
 if cls not in classes:
     exit(0)
 cls_id = classes.index(cls)
@@ -48,49 +48,50 @@ for txt_name in txt_name_list:
     """ Open input text files """
     txt_path = mypath + txt_name
     print("Input:" + txt_path)
-    txt_file = open(txt_path, "r")
-    lines = txt_file.read().split('\n')   #for ubuntu, use "\r\n" instead of "\n"
-    
-    """ Open output text files """
-    txt_outpath = outpath + txt_name
-    print("Output:" + txt_outpath)
-    txt_outfile = open(txt_outpath, "a")
-    
-    
-    """ Convert the data to YOLO format """
     ct = 0
-    for line in lines:
-        #print('lenth of line is: ')
-        #print(len(line))
-        #print('\n')
-        elems = line.split(' ')
-        if(len(elems) >= 2):
-        #if(len(line) >= 2):
-            ct = ct + 1
-            print(line + "\n")
+    if ".txt" in txt_path:
+        txt_file = open(txt_path, "r")
+        lines = txt_file.read().split('\n')   #for ubuntu, use "\r\n" instead of "\n"
+        
+        """ Open output text files """
+        txt_outpath = outpath + txt_name
+        print("Output:" + txt_outpath)
+        txt_outfile = open(txt_outpath, "a")
+        
+        
+        """ Convert the data to YOLO format """
+        for line in lines:
+            #print('lenth of line is: ')
+            #print(len(line))
+            #print('\n')
             elems = line.split(' ')
-            print(elems)
-            xmin = elems[0]
-            xmax = elems[2]
-            ymin = elems[1]
-            ymax = elems[3]
-            print(elems[0])
-            #
-            img_path = str('%s/Images/%s/%s.jpg'%(wd, cls, os.path.splitext(txt_name)[0]))
-            #t = magic.from_file(img_path)
-            #wh= re.search('(\d+) x (\d+)', t).groups()
-            im=Image.open(img_path)
-            w= int(im.size[0])
-            h= int(im.size[1])
-            #w = int(xmax) - int(xmin)
-            #h = int(ymax) - int(ymin)
-            # print(xmin)
-            print(w, h)
-            print(float(xmin), float(xmax), float(ymin), float(ymax))
-            b = (float(xmin), float(xmax), float(ymin), float(ymax))
-            bb = convert((w,h), b)
-            print(bb)
-            txt_outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+            if(len(elems) >= 2):
+            #if(len(line) >= 2):
+                ct = ct + 1
+                print(line + "\n")
+                elems = line.split(' ')
+                print(elems)
+                xmin = elems[0]
+                xmax = elems[2]
+                ymin = elems[1]
+                ymax = elems[3]
+                print(elems[0])
+                #
+                img_path = str('%s/Images/%s/%s.jpg'%(wd, cls, os.path.splitext(txt_name)[0]))
+                #t = magic.from_file(img_path)
+                #wh= re.search('(\d+) x (\d+)', t).groups()
+                im=Image.open(img_path)
+                w= int(im.size[0])
+                h= int(im.size[1])
+                #w = int(xmax) - int(xmin)
+                #h = int(ymax) - int(ymin)
+                # print(xmin)
+                print(w, h)
+                print(float(xmin), float(xmax), float(ymin), float(ymax))
+                b = (float(xmin), float(xmax), float(ymin), float(ymax))
+                bb = convert((w,h), b)
+                print(bb)
+                txt_outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
     """ Save those images with bb into list"""
     if(ct != 0):
--- main.py
+++ main.py
@@ -7,8 +7,17 @@
 #
 #-------------------------------------------------------------------------------
 from __future__ import division
-from Tkinter import *
-import tkMessageBox
+
+try:
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter 
+    import tkMessageBox
+except ImportError:
+    # for Python3
+    from tkinter import * 
+    import tkinter.messagebox
+
+
 from PIL import Image, ImageTk
 import os
 import glob
@@ -135,7 +144,7 @@ class LabelTool():
         print(self.imageDir)
         self.imageList = glob.glob(os.path.join(self.imageDir, '*.jpg'))
         if len(self.imageList) == 0:
-            print 'No .jpg images found in the specified dir!'
+            print ('No .jpg images found in the specified dir!')
             return
 
         # default to the 1st image in the collection
@@ -167,7 +176,7 @@ class LabelTool():
             self.egLabels[i].config(image = self.egList[-1], width = SIZE[0], height = SIZE[1])
 
         self.loadImage()
-        print '%d images loaded from %s' %(self.total, s)
+        print ('%d images loaded from %s' %(self.total, s))
 
     def loadImage(self):
         # load image
@@ -207,7 +216,7 @@ class LabelTool():
             print(len(self.bboxList))
             for bbox in self.bboxList:
                 f.write(' '.join(map(str, bbox)) + '\n')
-        print 'Image No. %d saved' %(self.cur)
+        print ('Image No. %d saved' %(self.cur))
 
 
     def mouseClick(self, event):
